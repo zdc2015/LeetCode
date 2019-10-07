c99715e710e6c364f89f7c91036fecb31a446897
zdc <274554594@qq.com>
2019-10-08 00:11:43 +0800

    nothing to do

---
+++ src/LinkedList/Solution.java
@@ -0,0 +1,22 @@
+package LinkedList;
+
+public class Solution {
+	
+	static class ListNode{
+		int value;
+		ListNode next;
+		ListNode(int x){
+			this.value = x;
+		}
+	}
+	
+	/*
+	 * 
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
